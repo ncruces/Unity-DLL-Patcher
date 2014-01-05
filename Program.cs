34859b5b3151e083c0a996daf9780cc960b18acb
Nuno Cruces <ncruces@gmail.com>
2014-01-05 01:37:19 +0000

    Keeps debug information where appropriate
    Location of ildasm obtained from different registry key.

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace PatchDlls
 
             Ildasm(assembly, il);
             var source = File.ReadAllText(il);
+            var debug = GetDebuggableAttribute(source);
             var builder = new StringBuilder();
 
             source = PatchInterlockedExchange(source);
@@ -45,26 +46,47 @@ namespace PatchDlls
             }
 
             File.WriteAllText(il, source);
-            Ilasm(il, res, assembly);
+            Ilasm(il, res, assembly, debug);
 
             File.Delete(il);
             File.Delete(res);
         }
 
-        static void Ilasm(string il, string res, string assembly)
+        static void Ilasm(string il, string res, string assembly, DebuggableAttribute debug)
         {
             var ilasm = Path.Combine(
                 Environment.GetFolderPath(Environment.SpecialFolder.Windows),
                 "Microsoft.NET", "Framework", "v2.0.50727", "ilasm.exe");
 
-            Run(ilasm, "\"{0}\" /resource=\"{1}\" /out=\"{2}\" {3}", il, res, assembly, Path.GetExtension(assembly).Replace('.', '/'));
+            // DLL or EXE
+            var args = Path.GetExtension(assembly).Replace('.', '/');
+
+            // Debugging
+            if (debug.IsJITOptimizerDisabled)
+            {
+                args += debug.DebuggingFlags.HasFlag(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints) ?
+                    " /debug=impl" :
+                    " /debug";
+            }
+            else if (debug.DebuggingFlags.HasFlag(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints))
+            {
+                args += " /debug=opt";
+            }
+
+            // Optimization
+            if (!debug.DebuggingFlags.HasFlag(DebuggableAttribute.DebuggingModes.DisableOptimizations))
+            {
+                args += " /optimize /fold";
+            }
+
+            Run(ilasm, "\"{0}\" /noautoinherit /resource=\"{1}\" /out=\"{2}\" {3}", il, res, assembly, args);
         }
 
         static void Ildasm(string assembly, string il)
         {
             var ildasm = Path.Combine(
-                Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SDKs\Windows", "CurrentInstallFolder", "") as string,
-                "Bin", "ildasm.exe");
+                Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SDKs\Windows\v7.0A\WinSDK-NetFx35Tools", "InstallationFolder", "") as string,
+                "ildasm.exe");
 
             Run(ildasm, "\"{0}\" /linenum /typelist /nobar /out=\"{1}\"", assembly, il);
         }
@@ -78,6 +100,15 @@ namespace PatchDlls
                 WindowStyle = ProcessWindowStyle.Hidden,
                 CreateNoWindow = true,
             }).WaitForExit();
+        }
+
+        static DebuggableAttribute GetDebuggableAttribute(string source)
+        {
+            var match = Regex.Match(source,
+                @".custom instance void \[mscorlib\]System.Diagnostics.DebuggableAttribute::.ctor\(valuetype \[mscorlib\]System.Diagnostics.DebuggableAttribute/DebuggingModes\) = " +
+                @"\( (..) (..) (..) (..) (..) (..) (..) (..) \)");
+            var builder = match.Groups.Cast<Group>().Skip(1).Reverse().Aggregate(new StringBuilder(16), (s, g) => s.Append(g.Value));
+            return new DebuggableAttribute((DebuggableAttribute.DebuggingModes)Convert.ToInt64(builder.ToString()));
         }
 
         static string PatchInterlockedExchange(string source)
